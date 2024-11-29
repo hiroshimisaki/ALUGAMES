6bc6812088bcc8423b9f6f210292520aeafbb0b1
Hiroshi Misaki <hiroshimisaki14@gmail.com>
2024-11-29 11:52:53 -0300

    adicionando contador de jogos alugados

--- js/app.js
+++ js/app.js
@@ -1,27 +1,40 @@
+let jogosAlugados = 0;
+
+function contarExibirJogosAlugados () {
+    console.log (`Total de jogos alugados: ${jogosAlugados}`);
+}
+
 //Criando uma função alterarStatus que corresponde ao botão alterarStatus no HTML
 function alterarStatus(id) {
-
-//Console.log apenas para verificar se o id corresponde ao do botão clicado
-    console.log(`o id é ${id}`);
-
+    
 //Declarando variaveis referentes aos elementos do HTML
-    let game = document.getElementById(`game-${id}`);
-    let imagem = game.querySelector(".dashboard__item__img");
-    let botao = game.querySelector(".dashboard__item__button");
+        let game = document.getElementById(`game-${id}`);
+        let imagem = game.querySelector(".dashboard__item__img");
+        let botao = game.querySelector(".dashboard__item__button");
+    
+//Criando if para alterar texto e cor do botão ao ser clicado e a imagem do jogo
+        if (botao.textContent === "Alugar") {
+            botao.textContent = "Devolver";
+            botao.classList.add("dashboard__item__button--return");
+            imagem.classList.add("dashboard__item__img--rented");
+            jogosAlugados++;
+            
+        } else {
+//Usando função confirm para pedir confirmação de devolução de jogo
+            if (confirm("Deseja mesmo devolver este jogo?")) {
+                botao.textContent = "Alugar";
+                botao.classList.remove("dashboard__item__button--return");
+                imagem.classList.remove("dashboard__item__img--rented");
+                jogosAlugados--;
+            }      
+        }
 
-//Criando if para alterar texto e cor do botão ao ser clicado
-    if (botao.textContent === "Alugar") {
-        botao.textContent = "Devolver";
-        botao.classList.add("dashboard__item__button--return");
-    } else {
-        botao.textContent = "Alugar";
-        botao.classList.remove("dashboard__item__button--return");
+        contarExibirJogosAlugados();
     }
 
-//Criando id para alterar a imagem ao ser clicado
-    if (imagem.classList.contains("dashboard__item__img--rented")) {
-        imagem.classList.remove("dashboard__item__img--rented");
-    } else {
-        imagem.classList.add("dashboard__item__img--rented");
-    }
-}
+//addEventListener para esperar o DOM estar totalmente carregado para então executar uma função anonima que irá igualar jogosAlugados com a quantidade de arquivos achados no HTML com .dashboard__item__img--rented e chamar a função contarExibirJogosAlugados
+//Funções anonimas são uteis para eventos especificos como nesse caso
+    document.addEventListener("DOMContentLoaded", function() {
+        jogosAlugados = document.querySelectorAll(".dashboard__item__img--rented").length;
+        contarExibirJogosAlugados();
+    });
